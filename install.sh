45bcd95bda17c290cf475773739c549a771c6868
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-07 13:38:02 -0400

    added newlines between messages in install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo INSTALLING DEPENDENCIES
+echo; echo INSTALLING DEPENDENCIES
 apt-get install \
   xresponse \
   x11-utils \
@@ -13,21 +13,21 @@ INSTALL_DIR=/opt/n9-button-monitor
 UPSTART_DIR=/etc/init/apps
 CONF_DIR=/home/user/.config
 
-echo COPYING EXEC TO $INSTALL_DIR
+echo; echo COPYING EXEC TO $INSTALL_DIR
 mkdir -p $INSTALL_DIR
 cp n9-button-monitor.py $INSTALL_DIR
 chmod +x $INSTALL_DIR/n9-button-monitor.py
 
-echo COPYING UPSTART SCRIPT TO $UPSTART_DIR
+echo; echo COPYING UPSTART SCRIPT TO $UPSTART_DIR
 cp n9-button-monitor.conf $UPSTART_DIR
 
 if [ -e $CONF_DIR/n9-button-monitor.ini ]; then
-  echo LEAVING EXISTING INI
+  echo; echo LEAVING EXISTING INI
 if [ -e $CONF_DIR/n9-button-monitor.conf ]; then
-  echo RENAMING n9-button-monitor.conf to n9-button-monitor.ini
+  echo; echo RENAMING n9-button-monitor.conf to n9-button-monitor.ini
   mv $CONF_DIR/n9-button-monitor.conf $CONF_DIR/n9-button-monitor.ini
 else
-  echo INSTALLING DEFAULT INI
+  echo; echo INSTALLING DEFAULT INI
   cp n9-button-monitor.ini $CONF_DIR
 fi
 
