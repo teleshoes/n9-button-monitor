af679c1c79a51dfab91aabe6677e3ecb3fe1787b
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-07 13:37:14 -0400

    fixed up msg printing and made ini replacement make sense

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-set -x
-
+echo INSTALLING DEPENDENCIES
 apt-get install \
   xresponse \
   x11-utils \
@@ -14,14 +13,21 @@ INSTALL_DIR=/opt/n9-button-monitor
 UPSTART_DIR=/etc/init/apps
 CONF_DIR=/home/user/.config
 
+echo COPYING EXEC TO $INSTALL_DIR
 mkdir -p $INSTALL_DIR
 cp n9-button-monitor.py $INSTALL_DIR
 chmod +x $INSTALL_DIR/n9-button-monitor.py
 
+echo COPYING UPSTART SCRIPT TO $UPSTART_DIR
 cp n9-button-monitor.conf $UPSTART_DIR
 
-mv $CONF_DIR/n9-button-monitor.conf $CONF_DIR/home/user/.config/n9-button-monitor.ini
-if [ ! -e $CONF_DIR/n9-button-monitor.ini ]; then
+if [ -e $CONF_DIR/n9-button-monitor.ini ]; then
+  echo LEAVING EXISTING INI
+if [ -e $CONF_DIR/n9-button-monitor.conf ]; then
+  echo RENAMING n9-button-monitor.conf to n9-button-monitor.ini
+  mv $CONF_DIR/n9-button-monitor.conf $CONF_DIR/n9-button-monitor.ini
+else
+  echo INSTALLING DEFAULT INI
   cp n9-button-monitor.ini $CONF_DIR
 fi
 
