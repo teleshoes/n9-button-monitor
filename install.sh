ecfd439d42bd36654e2af459799581811a3e531c
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-07 13:30:55 -0400

    added an install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+apt-get install \
+  xresponse \
+  x11-utils \
+  python-qmsystem \
+  python-qtmobility.multimediakit \
+  python-qtmobility.systeminfo \
+  python-pyside.qtgui \
+  python-pyside.qtcore \
+;
+
+INSTALL_DIR=/opt/n9-button-monitor
+UPSTART_DIR=/etc/init/apps
+CONF_DIR=/home/user/.config
+
+mkdir -p $INSTALL_DIR
+cp n9-button-monitor.py $INSTALL_DIR
+chmod +x $INSTALL_DIR/n9-button-monitor.py
+
+cp n9-button-monitor.conf $UPSTART_DIR
+
+mv $CONF_DIR/n9-button-monitor.conf $CONF_DIR/home/user/.config/n9-button-monitor.ini
+if [ ! -e $CONF_DIR/n9-button-monitor.ini ]; then
+  cp n9-button-monitor.ini $CONF_DIR
+fi
+
