ad60d645b114dd8c60a5892518699033ae182943
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-07 14:05:58 -0400

    run the python script in the background

--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@ sh /opt/n9-button-monitor/stop.sh
 /bin/develsh -c "
   source /etc/profile
   PATH=$PATH:/usr/local/bin
-  python /opt/n9-button-monitor/n9-button-monitor.py
+  python /opt/n9-button-monitor/n9-button-monitor.py &
 "
