2e82e3faf046ea317e66ebd4a42d6df71ab9449d
Elliot Wolk <elliot.wolk@gmail.com>
2012-05-06 14:14:00 -0400

    made sbox REALLY install dependencies

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-/scratchbox/login apt-get install build-essential cmake cmake-data
+echo; echo; echo will complain about emacsen-common and cmake-data
+echo; echo
+/scratchbox/login apt-get install build-essential cmake
+
+echo; echo; echo should fix install issues
+echo; echo
+/scratchbox/login apt-get install cmake-data
+/scratchbox/login apt-get install cmake
