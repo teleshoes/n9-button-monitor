488659004a5d1712439f5729ee97f12a2ba85cd6
Lcferrum <lcferrum@yandex.com>
2013-02-15 13:53:40 +0400

    New structure for debian package (continue)

--- scripts/build-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo; echo; echo will complain about emacsen-common and cmake-data
-echo; echo
-/scratchbox/login apt-get install build-essential cmake
-
-echo; echo; echo should fix install issues
-echo; echo
-/scratchbox/login apt-get install cmake-data
-/scratchbox/login apt-get install cmake
