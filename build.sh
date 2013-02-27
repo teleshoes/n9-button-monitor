82bd35c14d85f456eb6ca22957a5159880ca49bb
Elliot Wolk <elliot.wolk@gmail.com>
2013-02-27 02:47:49 -0500

    only initialized sbox and mount the project if its not already mounted

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@ project="n9-button-monitor"
 dir=$HOME/Code/$project
 mnt=/scratchbox/users/$USER/home/$USER/$project
 
-sudo /scratchbox/sbin/sbox_ctl start
-sudo /scratchbox/sbin/sbox_sync
+if ! mount | grep $project; then
+  sudo /scratchbox/sbin/sbox_ctl start
+  sudo /scratchbox/sbin/sbox_sync
 
-mkdir $mnt
-sudo mount -o bind $dir $mnt
+  mkdir -p $mnt
+  sudo mount -o bind $dir $mnt
+fi
 
 
 /scratchbox/login -d $HOME find -maxdepth 1 -name $project*.deb -delete
