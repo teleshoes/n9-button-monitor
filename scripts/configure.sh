955a314576e4bd498e06c869efedd3850a7f6aac
Elliot Wolk <elliot.wolk@gmail.com>
2013-02-27 02:41:01 -0500

    removed stupid build scripts

--- scripts/configure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-project="n9-button-monitor"
-dir="$HOME/$project/build"
-/scratchbox/login mkdir -p $dir
-/scratchbox/login -d $dir cmake ..
--- scripts/prune.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-project="n9-button-monitor"
-/scratchbox/login -d $HOME/$project/build rm -rf *
