2f64e5b900a6a4d350e5fcb0b42013ee4ac07082
Elliot Wolk <elliot.wolk@gmail.com>
2013-02-27 02:45:44 -0500

    combine mount+makedeb into build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 project="n9-button-monitor"
+dir=$HOME/Code/$project
+mnt=/scratchbox/users/$USER/home/$USER/$project
+
+sudo /scratchbox/sbin/sbox_ctl start
+sudo /scratchbox/sbin/sbox_sync
+
+mkdir $mnt
+sudo mount -o bind $dir $mnt
+
 
 /scratchbox/login -d $HOME find -maxdepth 1 -name $project*.deb -delete
 /scratchbox/login -d $HOME find -maxdepth 1 -name $project*.changes -delete
