59a59a863dd1f0b8c1290c46e405cf1caaf20088
Elliot Wolk <elliot.wolk@gmail.com>
2014-12-03 12:58:19 -0500

    dbusbtn: add support for multi-button patterns, remove repeat buffer

--- src/dbusbtn.py
+++ src/dbusbtn.py
@@ -10,11 +10,12 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-import time
 import signal
+import sys
+import threading
+import time
 from traceback import print_exc
- 
+
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 from PySide.QtCore import QCoreApplication
@@ -23,15 +24,19 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 class DbusButton():
   def __init__(self):
-    self.service="org.freedesktop.Hal"
-    self.path="/org/freedesktop/Hal/devices/computer_logicaldev_input_0"
-    self.interface="org.freedesktop.Hal.Device"
-    self.member="Condition"
-
-    self.repeatBufferMs = 800
-
-    self.lastClickMs = dict()
+    self.service = "org.freedesktop.Hal"
+    self.path = "/org/freedesktop/Hal/devices/computer_logicaldev_input_0"
+    self.interface = "org.freedesktop.Hal.Device"
+    self.member = "Condition"
+    self.delayedHandlerTimer = None
+    self.patternDelayMs = 1500
+    self.pattern = []
     self.handler = self.defaultHandler
+  def ensureTimer(self):
+    if self.delayedHandlerTimer != None:
+      self.delayedHandlerTimer.cancel()
+    delayS = self.patternDelayMs / 1000.0
+    self.delayedHandlerTimer = threading.Timer(delayS, self.delayedHandler)
   def defaultHandler(self, button):
     print >> sys.stderr, "button: " + button
   def setHandler(self, handler):
@@ -39,12 +44,12 @@ class DbusButton():
   def setRepeatBufferMs(self, repeatBufferMs):
     self.repeatBufferMs = repeatBufferMs
   def buttonClicked(self, btn):
-    nowMs = int(round(time.time() * 1000))
-    if btn in self.lastClickMs:
-      if nowMs - self.lastClickMs[btn] < self.repeatBufferMs:
-        print >> sys.stderr, "  ignored: " + btn
-        return
-    self.lastClickMs[btn] = nowMs
+    self.ensureTimer()
+    self.pattern.append(btn)
+    self.delayedHandlerTimer.start()
+  def delayedHandler(self):
+    btn = ",".join(self.pattern)
+    self.pattern = []
     self.handler(btn)
   def connectButtonDbus(self):
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
@@ -55,7 +60,7 @@ class DbusButton():
     except dbus.DBusException:
       print_exc()
       sys.exit(1)
- 
+
     iface.connect_to_signal(self.member,
       lambda cond, arg: self.buttonClicked(arg))
 
